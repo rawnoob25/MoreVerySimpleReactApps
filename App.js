fb679124085aebd991badb804ae6c4fce6d844cc
Raw Noob <rawnoob25@gmail.com>
2018-04-13 13:45:09 -0400

    make hello world with icon above it...use style= tag for icon

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+      <div><img src = {logo} alt = 'logo' height = '200' style={{display:'block'}}/>Hello World</div>
+    );
+  }
+}
+
+export default App;
