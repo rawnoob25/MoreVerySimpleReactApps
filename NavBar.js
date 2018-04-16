f9a0451dfdab4fc21e411035fe16524b792e1f78
Raw Noob <rawnoob25@gmail.com>
2018-04-16 16:23:12 -0400

    remove Elephant class def from NavBar.js

--- NavBar.js
+++ NavBar.js
@@ -12,17 +12,3 @@ export class NavBar extends React.Component {
     }
 }
 
-export class Elephant extends React.Component {
-    render(){
-        const picLink = 'https://upload.wikimedia.org/wikipedia/commons/3/37/African_Bush_Elephant.jpg';
-        return (
-            <div>
-                <figure>
-                    <img src={picLink} alt='Elephant' height='350'/>
-                    <figcaption>A female elephant in Tanzania</figcaption>
-                </figure>
-                <p>This beast has a top speed of {this.props.specs.speed}</p>
-            </div>
-        );
-    }
-}
