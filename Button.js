7577b1692c3e556be1bb4c619d177870a1949fef
Raw Noob <rawnoob25@gmail.com>
2018-04-16 17:08:15 -0400

    apply style to button

--- Button.js
+++ Button.js
@@ -5,7 +5,7 @@ export class Button extends React.Component {
     render(){
         return (
             <div>
-                <button onClick = {this.props.clickHandler}>Toggle elephant weight and height.</button>
+                <button id='theButton' onClick = {this.props.clickHandler}>Toggle elephant weight and height.</button>
             </div>
         );
     }
